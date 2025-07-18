d54774c729686fac74bd9bce04a8f5d31c8e0ed1
Yassir aittata <yassir.aittata@gmail.com>
2025-07-18 17:10:55 +0100

    include and exclude users from a project

--- backend/src/controllers/project.ts
+++ backend/src/controllers/project.ts
@@ -54,4 +54,72 @@ const deleteProject: express.RequestHandler = async (req, res, next) => {
   });
 };
 
-export { createProject, deleteProject };
+const assignUsersToProject: express.RequestHandler = async (req, res, next) => {
+  const projectId = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+    return res.status(400).json({ message: "Invalid ID format" });
+  }
+
+  const project = await projectModel.findById(projectId);
+
+  if (!project) {
+    res.status(404);
+    return next(Error("Project not found"));
+  }
+
+  if (project.creator.toString() !== req.user.id) {
+    res.status(403);
+    return next(Error("You do not have permission to update this project"));
+  }
+
+  const { developers } = req.body;
+
+  project.developers = [...project.developers, ...developers];
+  await project.save();
+  res.status(200).json({
+    message: "Users assigned to project successfully",
+    project,
+  });
+};
+
+const eremoveUsersFromProject: express.RequestHandler = async (
+  req,
+  res,
+  next
+) => {
+  const projectId = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+    return res.status(400).json({ message: "Invalid ID format" });
+  }
+
+  const project = await projectModel.findById(projectId);
+
+  if (!project) {
+    res.status(404);
+    return next(Error("Project not found"));
+  }
+
+  if (project.creator.toString() !== req.user.id) {
+    res.status(403);
+    return next(Error("You do not have permission to update this project"));
+  }
+
+  project.developers = project.developers.filter(
+    (dev) => !req.body.developers.includes(dev.toString())
+  );
+
+  await project.save();
+  res.status(200).json({
+    message: "Users was removed from the project successfully",
+    project,
+  });
+};
+
+export {
+  createProject,
+  deleteProject,
+  assignUsersToProject,
+  eremoveUsersFromProject,
+};
--- backend/src/router/projects.ts
+++ backend/src/router/projects.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import validateToken from "../middleware/validateTokenHandler.ts";
-import { createProject, deleteProject } from "../controllers/project.ts";
+import {
+  createProject,
+  deleteProject,
+  assignUsersToProject,
+  eremoveUsersFromProject,
+} from "../controllers/project.ts";
 
 export default (router: express.Router) => {
   router.post("/projects/create", validateToken, createProject);
   router.delete("/projects/:id", validateToken, deleteProject);
+  router.put("/projects/assign/:id", validateToken, assignUsersToProject);
+  router.put("/projects/remove/:id", validateToken, eremoveUsersFromProject);
 };
